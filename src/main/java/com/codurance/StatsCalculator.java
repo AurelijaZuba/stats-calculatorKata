8f95176421297f7d44cdd1c6b0e6cd4cf3cc5bb0
AurelijaZuba <auryte00@hotmail.com>
2019-06-27 10:55:38 +0100

    Failing test for single value when calculating minimum value

---
+++ src/main/java/com/codurance/StatsCalculator.java
@@ -0,0 +1,16 @@
+package com.codurance;
+
+import java.util.List;
+
+public class StatsCalculator {
+    private List<Integer> numbers;
+
+    public StatsCalculator(List<Integer> numbers) {
+
+        this.numbers = numbers;
+    }
+
+    public int calculateMinimum() {
+        throw new UnsupportedOperationException();
+    }
+}
--- src/test/java/com/codurance/StatsCalculatorShould.java
+++ src/test/java/com/codurance/StatsCalculatorShould.java
@@ -1,4 +1,24 @@
 package com.codurance;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class StatsCalculatorShould {
+
+    @Test
+    void return_single_value_for_minimum_value() {
+        List<Integer> stats = new ArrayList<>();
+        stats.add(0);
+        int expected = 0;
+
+        StatsCalculator statsCalculator = new StatsCalculator(stats);
+
+        int result = statsCalculator.calculateMinimum();
+
+        assertThat(result).isEqualTo(expected);
+    }
 }
